d8f0e65b7f1570f7974c0d1b11dfded99e2f83af
Brageanth David Palencia Avila <brageanth@gmail.com>
2018-09-05 20:42:40 -0500

    Deploy 2

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/JAZZ'));
+app.use(express.static(__dirname + '/dist/Jazz'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/JAZZ/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Jazz/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
